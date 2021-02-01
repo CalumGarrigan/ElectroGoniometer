473b34c494fb57b525a07aed8a8d8cd80718d901
Calum Garrigan <calum@garrigan.us>
2021-01-31 18:24:28 -0800

    Added new Screens (Activities) for "New Patient", "Measurement", "List of Patients" and "Patient Details". Added  stubbed button on Main screen.

---
+++ app/src/main/java/com/garrigan/calum/ljm/electrogoniometer/NewPatientActivity.java
@@ -0,0 +1,13 @@
+package com.garrigan.calum.ljm.electrogoniometer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewPatientActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO setContentView(R.layout.activity_main);
+    }
+}
