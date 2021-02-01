063b9753ded7b264ff1f189ef6f9c5b155dfbba1
Calum Garrigan <calum@garrigan.us>
2021-01-31 18:22:45 -0800

    Added new Screens (Activities) for "New Patient", "Measurement", "List of Patients" and "Patient Details". Added  stubbed button on Main screen.

--- app/src/main/java/com/garrigan/calum/ljm/electrogoniometer/MainActivity.java
+++ app/src/main/java/com/garrigan/calum/ljm/electrogoniometer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.garrigan.calum.ljm.electrogoniometer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupButtonClicks();
+    }
+
+    private void setupButtonClicks() {
+        TextView btnNewPatient = findViewById(R.id.button_new_patient);
+        if(btnNewPatient != null) {
+            btnNewPatient.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "New Patient pressed", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        TextView btnPatientData = findViewById(R.id.button_patient_data);
+        if(btnPatientData != null) {
+            btnPatientData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Patient Data pressed", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        TextView btnAbout = findViewById(R.id.button_about_app);
+        if(btnAbout != null) {
+            btnAbout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "About pressed", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
